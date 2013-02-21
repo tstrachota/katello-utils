106440725b42bf0e51fc9aa43fbb0d8d1ee67535
Tomas Strachota <tstrachota@redhat.com>
2013-02-21 15:19:56 +0100

    test data - fix in chrome

--- katello_fill/test_data/chrome.sh
+++ katello_fill/test_data/chrome.sh
@@ -14,9 +14,9 @@ fi
 
 wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O $TMP_KEY 
 
-$CMD gpg_key create --name="google_key" --file="$TMP_KEY"
+$CMD gpg_key create --name="google_key" --file="$TMP_KEY" --org="$ORG"
 $CMD provider create --name="google" --org="$ORG"
 $CMD product create --name="chrome" --provider="google" --org="$ORG" --gpgkey="google_key"
-$CMD repo create --name="chrome_x86" --url="$REPO" --product="chrome"
+$CMD repo create --name="chrome_x86" --url="$REPO" --product="chrome" --org="$ORG"
 
 rm $TMP_KEY 
